ddc55768a790e63b4ed43bb44b990ae50974a347
Ismet Ferdi SEN <ismetferdi@gmail.com>
2021-10-15 15:47:10 +0200

    I would like to finish adding part. You do not have to give point it...

--- 04_exercise/script.js
+++ 04_exercise/script.js
@@ -1,8 +1,10 @@
 var books = [
-    {title: 'title1', author:'author1', stock:'in stock', price:'1 CHF'},
-    {title: 'title2', author:'author2', stock:'sold out', price:'2 CHF'},
-    {title: 'title3', author:'author3', stock:'in stock', price:'3 CHF' }
+    {title: 'title2', author:'author1', stock:'in stock', price:'1 CHF'},
+    {title: 'title1', author:'author2', stock:'sold out', price:'2 CHF'},
+    {title: 'title0', author:'author3', stock:'in stock', price:'3 CHF' }
 ];
+
+books.sort((a, b) => a.title.localeCompare(b.title))
 
 var html = books.map(function (element) {
     return '<li>' + element.title + ', '
@@ -11,13 +13,24 @@ var html = books.map(function (element) {
                   + element.price + ', ' + '</li>';  
   }).join('');
 
+
 document.getElementById("getIt").innerHTML = html;
 
-function addBook(titleN,authorN,stockN,priceN){
+function addBook(){
     const todo = new Object();
-                todo.title=titleN;
-                todo.author=authorN;
-                todo.stock=stockN;
-                todo.price=priceN;
+                todo.title=prompt(`Title of book:`);
+                todo.author=prompt(`Author:`);
+                todo.stock=prompt('in stock or sold out?');
+                todo.price=prompt('price? in CHF')
     books.push(todo);
+    books.sort((a, b) => a.title.localeCompare(b.title))
+    
+                   var html = books.map(function (element) {
+                    return '<li>' + element.title + ', '
+                                  + element.author + ', '
+                                  + element.stock + ', '
+                                  + element.price + ', ' + '</li>';  
+                  }).join('');
+                
+                document.getElementById("getIt").innerHTML = html;
 }
